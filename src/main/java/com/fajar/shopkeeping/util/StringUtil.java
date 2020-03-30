12363315be4955a18062d86cfb10db04485c45a7
Fajar AM <somabangsa@gmail.com>
2020-03-30 22:09:12 +0700

    add util

--- src/main/java/com/fajar/shopkeeping/util/StringUtil.java
+++ src/main/java/com/fajar/shopkeeping/util/StringUtil.java
@@ -12,7 +12,7 @@ public class StringUtil {
 		if (length < 1) {
 			length = 1;
 		}
-		
+
 		for (int i = 0; i < length; i++) {
 
 			Integer n = rand.nextInt(9);
@@ -34,7 +34,7 @@ public class StringUtil {
 	}
 
 	public static String buildString(Object... strings) {
-		
+
 		StringBuilder stringBuilder = new StringBuilder();
 
 		for (Object string : strings) {
@@ -46,6 +46,33 @@ public class StringUtil {
 
 	public static String doubleQuoteMysql(String str) {
 		return " `".concat(str).concat("` ");
+	}
+
+	public static String beautifyNominal(Integer Int) {
+		String[] rawNominal = Int.toString().split("\\.");
+		String nominal = rawNominal[0];
+		String hasil = "";
+		if (nominal.length() > 3) {
+			int nol = 0;
+			for (int i = nominal.length() - 1; i > 0; i--) {
+				nol++;
+				hasil = nominal.charAt(i) + hasil;
+				if (nol == 3) {
+					hasil = "." + hasil;
+					nol = 0;
+				}
+
+			}
+			hasil = nominal.charAt(0) + hasil;
+		} else {
+			hasil = Int.toString();
+		}
+
+		if (rawNominal.length > 1) {
+			hasil = hasil + "," + rawNominal[1];
+		}
+
+		return hasil;
 	}
 
 }
